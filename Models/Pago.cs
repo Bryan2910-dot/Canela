5fae5eed36de16a45af3ba4450cda31c9760da49
Bry <bryan2910arnold@gmail.com>
2025-06-08 01:48:36 -0500

    model pago

---
+++ Models/Pago.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Canela.Models
+{
+    [Table("t_pago")]
+    public class Pago
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public decimal MontoTotal { get; set; }
+
+        public DateTime PaymentDate { get; set; } = DateTime.UtcNow;
+
+        public string? Status { get; set; }
+
+        // Relación con el usuario (AspNetUsers)
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public IdentityUser? User { get; set; }
+
+        // Relación con la orden
+        [ForeignKey("Order")]
+        public int? OrderId { get; set; }
+        public Orden? Order { get; set; }
+
+        // Opcional: campos para PayPal
+        public string? PayPalPaymentId { get; set; }
+        public string? PayPalPayerId { get; set; }
+    }
+}
