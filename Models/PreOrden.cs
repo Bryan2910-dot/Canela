76b07ba1e8e4685014c7d4a3b632fceb7facec12
Bry <bryan2910arnold@gmail.com>
2025-06-08 01:48:57 -0500

    model preorden

---
+++ Models/PreOrden.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Canela.Models
+{
+    [Table("t_preorden")]
+    public class PreOrden
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+        public Producto? Producto { get; set; }
+        public int Cantidad { get; set; }
+        [NotNull]
+        public Decimal Precio { get; set; }
+        public string Status { get; set; } = "PENDIENTE";
+
+        // Agrega esta propiedad para almacenar el ID del usuario
+        public string UserId { get; set; }
+    }
+}
