2eade06b203eb42ee745303004f3fce0511536c2
Bry <bryan2910arnold@gmail.com>
2025-06-12 17:30:46 -0500

    cambios

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Threading.Tasks;
-
+using System.Globalization;
 
 namespace Canela.Models
 {
@@ -15,12 +13,26 @@ namespace Canela.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        
         [NotNull]
         public string? Name { get; set; }
+        
+        // Columna real en la base de datos (texto)
+        [Column("Price", TypeName = "text")]
         [NotNull]
-        public int Price { get; set; }
+        public string? PriceText { get; set; }
+        
+        // Propiedad no mapeada para trabajar con el valor numérico
+        [NotMapped]
+        public int Price 
+        { 
+            get => int.TryParse(PriceText, out var result) ? result : 0;
+            set => PriceText = value.ToString();
+        }
+        
         [NotNull]
         public string? Status { get; set; }
+        
         [NotNull]
         public string? ImageURL { get; set; }
     }
